ef94d5340ab7baeca885e1ba4b0234f193213636
trannhathoang8678 <trannhathoang8678@gmail.com>
2020-08-10 08:31:45 +0700

    Counter Model

--- src/database/models/Counter.java
+++ src/database/models/Counter.java
@@ -1,2 +1,30 @@
-package database.models;public class Counter {
+package database.models;
+
+public class Counter {
+    private String maker;
+    private int quantity;
+
+    public Counter(String maker, int quantity) {
+        this.maker = maker;
+        this.quantity = quantity;
+    }
+
+    public Counter() {
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
