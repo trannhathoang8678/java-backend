3e6f2a19131e03c2eea30628cd2e50e6a3c68d4b
trannhathoang8678 <trannhathoang8678@gmail.com>
2020-08-12 16:06:03 +0700

    add function updateNoOfStudentsInDepartment

--- src/database/StudentManagement.java
+++ src/database/StudentManagement.java
@@ -1,4 +1,35 @@
 package database;
 
+import database.services.LaptopService;
+import database.services.StudentService;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class StudentManagement {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL Drive Driver Not Found");
+            return;
+        }
+        System.out.println("MySQL JDBC Registered");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/manage_student","root","hoangnt");
+            System.out.println("SQL connection to Database established!");
+            StudentService studentService = new StudentService(connection);
+//            studentService.setDefaultToNoOfStudentsInDepartment();
+            studentService.turnOffSafeMode();
+            studentService.updateNoOfStudentsInDepartment();
+            connection.close();
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Connection Fail! Exception: " + e);
+            return;
+        }
+    }
 }
--- src/database/services/StudentService.java
+++ src/database/services/StudentService.java
@@ -1,5 +1,54 @@
 package database.services;
 
-public class StudentService {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 
+public class StudentService {
+    private Connection connection;
+
+    public StudentService(Connection connection) {
+        this.connection = connection;
+    }
+
+    public StudentService() {
+    }
+    public void turnOffSafeMode()
+    {
+        String sql = "SET sql_safe_updates = 0";
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateNoOfStudentsInDepartment() {
+        String sql = "SELECT COUNT(*),deptID FROM students GROUP BY deptID;";
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet deptIDrs = statement.executeQuery(sql);
+            while(deptIDrs.next())
+            {
+                sql = "UPDATE departments SET NoOfStudents = " + deptIDrs.getInt(1)
+                        + " WHERE DeptID = '" + deptIDrs.getString(2) + "' ;";
+                Statement statement1 = connection.createStatement();
+                statement1.executeUpdate(sql);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+         sql = "UPDATE departments SET NoOfStudents = 0 WHERE NoOfStudents IS NULL";
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
